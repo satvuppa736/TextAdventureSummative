593cc8e65ea8ab9c1e7e8d2280748e179130e7a3
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-11-07 12:33:34 -0500

    Commit October 07 2016 12:33pm

--- TextAdventureSummative/Form1.Designer.cs
+++ TextAdventureSummative/Form1.Designer.cs
@@ -29,39 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.titleLabel = new System.Windows.Forms.Label();
-            this.subtitleLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // titleLabel
+            // outputLabel
             // 
-            this.titleLabel.Font = new System.Drawing.Font("Trajan Pro", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(12, 9);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(352, 60);
-            this.titleLabel.TabIndex = 0;
-            this.titleLabel.Text = "ESCAPE FROM THE LOST NATION";
-            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // subtitleLabel
-            // 
-            this.subtitleLabel.Font = new System.Drawing.Font("Trajan Pro", 10F);
-            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
-            this.subtitleLabel.Location = new System.Drawing.Point(12, 69);
-            this.subtitleLabel.Name = "subtitleLabel";
-            this.subtitleLabel.Size = new System.Drawing.Size(352, 60);
-            this.subtitleLabel.TabIndex = 1;
-            this.subtitleLabel.Text = "A TEXT ADVENTURE";
-            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(16, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 198);
-            this.label1.TabIndex = 2;
+            this.outputLabel.Location = new System.Drawing.Point(16, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(348, 198);
+            this.outputLabel.TabIndex = 2;
             // 
             // Form1
             // 
@@ -69,9 +45,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(376, 456);
-            this.Controls.Add(this.subtitleLabel);
-            this.Controls.Add(this.titleLabel);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
             this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -81,10 +55,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label titleLabel;
-        private System.Windows.Forms.Label subtitleLabel;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- TextAdventureSummative/Form1.cs
+++ TextAdventureSummative/Form1.cs
@@ -12,9 +12,71 @@ namespace TextAdventureSummative
 {
     public partial class Form1 : Form
     {
+        int scene = 0;
+        Random randGen = new Random();
+        int attackValue, stealthValue;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+
+                else if (scene == 3)
+                {
+                    attackValue = randGen.Next(1, 11);
+                    if (attackValue >= 4)
+                    {
+                        scene = 6;
+                    }
+                    else if (attackValue < 4)
+                    {
+                        scene = 5;
+                    }
+                }
+                else if (scene == 6)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7 || scene == 9)
+                {
+                    scene = 10;
+                } 
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 12 || scene == 14)
+                {
+                    scene = 15;
+                }
+                else if (scene == 15)
+                {
+                    scene = 17;
+                }
+                else if (scene == 17)
+                {
+
+                }
+                    
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+
+            }
         }
     }
 }
